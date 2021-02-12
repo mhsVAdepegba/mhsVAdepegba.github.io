b148068a397669c11e63746598cd3f4ae02de332
mhsVAdepegba <78811933+mhsVAdepegba@users.noreply.github.com>
2021-02-12 17:30:18 -0500

    Add files via upload

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,37 +1,23 @@
-
-var circleSize = 30
-
-/* Mouse following using automatic function calls.
+/* Mouse following using built-in variables
 Courtney Edwards 2018-09-01*/
 
+
 function setup() {
-  var firstCanvas = createCanvas(600, 600);  
-  // create a square window for drawing
-  //firstCanvas.parent('sketch1'); 
-  // Connect with the <div id="sketch1">
+  var secondCanvas = createCanvas(600, 600);  // create a square window for drawing
+  //secondCanvas.parent('sketch2');  // Connect with the <div id="sketch2">
 }
+
 function draw() {
-  // This function does nothing, but it must exist.
-  
+  fill(255,255,255);
+  rect(0,0,600,600);
+  line(mouseX, mouseY, 50, 100);
+  // Draw a circle
+  if (mouseIsPressed) {
+    // When the mouse button is pressed
+    // change the colour randomly
+    
+    stroke(random(255), random(255), random(255));
+    
+  } 
 
- 
 }
-function mouseMoved() {
-  // This function is called whenever the mouse is moved
-  
-  // Draw a lolipop
- 
-  ellipse(mouseX, mouseY, circleSize, circleSize);
-  line(mouseX,mouseY,mouseX + 30,mouseY + 30)
-
-  
-}
- function mouseClicked() {
-  // This function is called whenever the mouse is clicked
-  // (but the mouse must not be moving at the same time)
-  // and will change the fill colour randomly
-  fill(random(255), random(255), random(255));
-   circleSize = circleSize + random(3)
-}
-
-
