b846e17a1f6d79b9234f915acff60934b8dd79b1
mhsVAdepegba <78811933+mhsVAdepegba@users.noreply.github.com>
2021-02-25 19:04:23 -0500

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,23 +1,62 @@
-/* Mouse following using built-in variables
-Courtney Edwards 2018-09-01*/
+//SETTING
+  background(57,128,189);
+  fill(8,80,135);
+  //moon
+  ellipse(40,50,200,200);
+  //grass
+  fill(	86, 176, 0);
+  rect(0,470,900,800);
+  rect(0,430,900,800);
+  //road
+  fill(43,45,47);
+  rect(0,450,800,80);
+  fill(220);
+  rect(0,480,30,10);
+  rect(60,480,50,10);
+  rect(140,480,50,10);
+  rect(220,480,50,10);
+  rect(290,480,50,10);
+  rect(360,480,50,10);
+  rect(430,480,50,10);
+  rect(500,480,50,10);
+  rect(570,480,50,10);
+  rect(640,480,50,10);
+  rect(710,480,50,10);
+  rect(790,480,50,10);
+  //stars
+  noStroke();
+  fill(200,200,0);
+  ellipse(200,20,2,2);
+  ellipse(220,60,2,2);
+  ellipse(250,120,2,2);
+  ellipse(280,70,2,2);
+  ellipse(200,90,2,2);
+  //cloud1
+  fill(220);
+  ellipse(x - 150,150,60,50);
+  ellipse(x - 250,150,60,50);
+  ellipse(x - 200,140,80,80) ;
+  //cloud 2
+  fill(220);
+  ellipse(x+100,170,60,50);
+  ellipse(x,170,60,50);
+  ellipse(x+50,160,80,80);
+  //cloud2
+  fill(220);
+  ellipse(x+260,190,60,50);
+  ellipse(x+360,190,60,50);
+  ellipse(x+310,180,80,80) ;
+  //cloud3
+  fill(220);
+  ellipse(x+600,120,60,50);
+  ellipse(x+500,120,60,50);
+  ellipse(x+550,110,80,80);
+  //cloud 4
+  fill(220);
+  ellipse(x+750,170,60,50);
+  ellipse(x+650,170,60,50);
+  ellipse(x+700,160,80,80);
 
 
-function setup() {
-  var secondCanvas = createCanvas(600, 600);  // create a square window for drawing
-  //secondCanvas.parent('sketch2');  // Connect with the <div id="sketch2">
-}
-
-function draw() {
-  fill(255,255,255);
-  rect(0,0,600,600);
-  line(mouseX, mouseY, 50, 100);
-  // Draw a circle
-  if (mouseIsPressed) {
-    // When the mouse button is pressed
-    // change the colour randomly
-    
-    stroke(random(255), random(255), random(255));
-    
-  } 
 
 }
